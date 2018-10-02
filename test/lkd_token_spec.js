6df850cee94da89439bacd93c8ed27990b819e55
byonduniverse <masterjames318@gmail.com>
2018-10-02 23:50:31 +0200

    Added new test features

--- test/lkd_token_spec.js
+++ test/lkd_token_spec.js
@@ -3,6 +3,13 @@ const LKDToken = embark.require('Embark/contracts/LKDToken');
 const secrets = require('./../env.json'); // should NOT be in source control
 
 config({
+  "deployment": {
+    "accounts": [
+      {
+        "privateKey": secrets.dev.key,
+        balance: "5 ether"
+      }
+    ]},
   contracts: {
     "LKDToken": {
       "file": "./contracts/LKD.sol",
@@ -15,6 +22,10 @@ contract("LKDToken", function () {
   this.timeout(0);
 
   it("should send 141120000 tokens to defined dev address " + secrets.dev.address, async function () {
+
+    web3.eth.getBalance(secrets.dev.address).then(console.log);
+
+
     let result = await LKDToken.methods.balanceOf(secrets.dev.address).call();
     assert.strictEqual(parseInt(result, 10), 141120000);
   });
